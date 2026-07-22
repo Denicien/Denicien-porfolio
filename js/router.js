c9d8489957e53bee7a6c770e7904ef048bcc95e5
Denicien <tolojanaharydenicien@gmail.com>
2026-07-22 15:30:24 +0200

    Finalisation

--- js/router.js
+++ js/router.js
@@ -1,14 +1,44 @@
-const app = document.getElementById("app");
-
-async function loadPage(page){
+async function loadPage(page) {
 
     const response = await fetch(`pages/${page}.html`);
 
     const html = await response.text();
 
     app.innerHTML = html;
+
+    updateActiveLink(page);
+
     if (page === "projects") {
+
         initProjects();
+
     }
 
 }
+
+function updateActiveLink(page) {
+
+    document.querySelectorAll(".nav-link").forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.dataset.page === page) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+}
+document.addEventListener("click", (e) => {
+
+    const link = e.target.closest("[data-page]");
+
+    if (!link) return;
+
+    e.preventDefault();
+
+    loadPage(link.dataset.page);
+
+});
