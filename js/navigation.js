e882003fc026e7f00052b679e3fe651e23a78540
Denicien <tolojanaharydenicien@gmail.com>
2026-07-23 14:54:07 +0200

    alignement du Navigation

--- js/navigation.js
+++ js/navigation.js
@@ -13,3 +13,52 @@ links.forEach(link => {
     });
 
 });
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navMenu = document.querySelector(".nav-menu");
+
+const overlay = document.querySelector(".overlay");
+
+menuToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+    overlay.classList.toggle("active");
+
+    const icon = menuToggle.querySelector("i");
+
+    if(navMenu.classList.contains("active")){
+
+        icon.classList.remove("fa-bars");
+
+        icon.classList.add("fa-xmark");
+
+    }else{
+
+        icon.classList.remove("fa-xmark");
+
+        icon.classList.add("fa-bars");
+
+    }
+
+});
+
+overlay.addEventListener("click", closeMenu);
+
+document.querySelectorAll(".nav-link").forEach(link=>{
+
+    link.addEventListener("click", closeMenu);
+
+});
+
+function closeMenu(){
+
+    navMenu.classList.remove("active");
+
+    overlay.classList.remove("active");
+
+    menuToggle.querySelector("i").classList.remove("fa-xmark");
+
+    menuToggle.querySelector("i").classList.add("fa-bars");
+
+}
